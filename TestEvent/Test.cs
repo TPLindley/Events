e4d0dbd6277bd5e4141c6606dfabd8fdd7e45d44
Tom Lindley <tom@solomotechnology.com>
2016-11-30 00:07:50 -0600

    Added TestEvent

---
+++ TestEvent/Test.cs
@@ -0,0 +1,27 @@
+﻿using EventBase;
+
+namespace TestEvent
+{
+    public class Test : BaseEvent
+    {
+        /// <summary>
+        /// Constructor to implement rules for 'Test' event
+        /// 
+        /// BaseEvent handles processing, this constructor sets the multiples and values to return
+        /// </summary>
+        public Test()
+        {
+            Multiples.Add(4, TestLibConstants.Test);
+            Multiples.Add(11, TestLibConstants.Patient);
+        }
+        public override string Name()
+        {
+            return TestLibConstants.Name;
+        }
+
+        public override string Process(int value)
+        {
+            return base.Process(value).Trim();
+        }
+    }
+}
---
+++ TestEvent/TestLibConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace TestEvent
+{
+    /// <summary>
+    ///  Constants for 'TestEvent' handler
+    /// </summary>
+    public static class TestLibConstants
+    {
+        public static string Name = "Test";
+        public static string Test = "Test";
+        public static string Patient = " Patient";
+    }
+}
