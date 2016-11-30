df09900ac5d4b5400f611f5b9bf4db2cf34840c4
Tom Lindley <tom@lindley.com>
2016-11-30 10:53:56 -0600

    Small change in ETW logger

--- EventBase/Logger/EtwLogger.cs
+++ EventBase/Logger/EtwLogger.cs
@@ -1,16 +1,13 @@
 ﻿using EventBase.Interfaces;
 using EventBase.Models;
-using System;
 using System.Diagnostics.Tracing;
-using System.Threading.Tasks;
-// ReSharper disable UnusedMember.Local
 
 namespace EventBase.Logger
 {
     /// <summary>
     /// Example logging function. Was implemented with WinRT ETW, but would not convert
     /// </summary>
-    [EventSource(Name = "EventsLog", Guid= "{fa17d3b0-0c84-488d-9805-4e27a7cdbbc9}")]
+    [EventSource(Name = "EventsLog", Guid= "{eeeeef10-0ffd-4b70-b8c6-1977f436b72e}")]
     public class EtwLogger : EventSource, ILog
     {
         #region Public
@@ -19,29 +16,12 @@ namespace EventBase.Logger
         }
         public void Log(LogEntry logEntry)
         {
-            WriteEvent(_eventId++, logEntry.ToString());
+            Write(logEntry.ToString());
         }
         public void Stop()
         {
             Dispose();
         }
-        #endregion
-        #region Private
-        private int _eventId = 1;
-        private string GetTimeStamp()
-        {
-            DateTime now = DateTime.Now;
-            return string.Format(System.Globalization.CultureInfo.InvariantCulture,
-                                 "{0:D2}{1:D2}{2:D2}-{3:D2}{4:D2}{5:D2}",
-                                 now.Year - 2000,
-                                 now.Month,
-                                 now.Day,
-                                 now.Hour,
-                                 now.Minute,
-                                 now.Second);
-        }
-
-
         #endregion
     }
 }
